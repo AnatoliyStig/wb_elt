9ef92f66ef5625546cdb64b8cc9ef223a2cff85d
Anatoliy Lysykh <anatoliy017@yandex.ru>
2024-06-02 16:46:57 +0300

    + new seller

--- dags/extract_sales.py
+++ dags/extract_sales.py
@@ -4,25 +4,20 @@ import requests
 from datetime import datetime
 
 date_from = "2023-01-01"
-token = "eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjMxMDI1djEiLCJ0eXAiOiJKV1QifQ.eyJlbnQiOjEsImV4cCI6MTcxNzY5Mzc2OCwiaWQiOiI3MDdhNjFkZC01MWUwLTRjNDktYjNhZC02N2Y0Y2VlYmQyOGQiLCJpaWQiOjUzNzQ1NTQ1LCJvaWQiOjQ0MTgyMCwicyI6MTA3Mzc0MTg1Niwic2lkIjoiMDliZTA5NTUtM2ViZC00MDNkLWJkNzctNGQwYjhmNzBkZjRiIiwidWlkIjo1Mzc0NTU0NX0.niVswDAdI503OlyTG0d6eCYZZP1f0wmGURj_y-PsWP0PdNzvZAqKw_4DpSuvfRqkn0_2EJcMJ2X48yd-0yow3Q"
+token_seller_1 = "you need write your Token"
+token_seller_2 = "you need write your Token"
 
 
 def main():
-    response_sale = requests.get(
-        "https://statistics-api.wildberries.ru/api/v1/supplier/sales",
-        headers={"Authorization": token},
-        params={"dateFrom": date_from},
-    )
-    data_sale = response_sale.json()
-
-    # connect
+    # connect to DB
     connection = psycopg2.connect(
         host=host, user=user, password=password, port=port, database=db_name
     )
 
-    # cursor
+    # create Cursor
     cursor = connection.cursor()
 
+    # if table not exist we should create it
     cursor.execute(
         """create table if not exists sales (
                     srid VARCHAR(100),
@@ -37,13 +32,23 @@ def main():
                     subject VARCHAR(50),
                     category VARCHAR(50),
                     brand VARCHAR(50),
+                    owner VARCHAR(20),
 					ctl_datetime TIMESTAMP WITHOUT TIME ZONE
                 )"""
     )
 
     cursor.execute("truncate table sales")
 
-    for item in data_sale:
+    # first load seller_1 data
+    response_order_seller_1 = requests.get(
+        "https://statistics-api.wildberries.ru/api/v1/supplier/orders",
+        headers={"Authorization": token_seller_1},
+        params={"dateFrom": date_from},
+    )
+
+    data_order_seller_1 = response_order_seller_1.json()
+
+    for item in data_order_seller_1:
         if isinstance(item, dict):
             srid = item.get("srid")
             gNumber = item.get("gNumber")
@@ -57,12 +62,13 @@ def main():
             subject = item.get("subject")
             category = item.get("category")
             brand = item.get("brand")
+            owner = "seller_1"
             ctl_datetime = datetime.now()
 
             cursor.execute(
                 "INSERT INTO sales (srid, gNumber, date_order, lastchangedate, \
                                     priceWithDisc,discountPercent, supplierArticle, isCancel, \
-                                    cancelDate, subject, category, brand, ctl_datetime) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                                    cancelDate, subject, category, brand, owner, ctl_datetime) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
                 (
                     srid,
                     gNumber,
@@ -76,6 +82,55 @@ def main():
                     subject,
                     category,
                     brand,
+                    owner,
+                    ctl_datetime,
+                ),
+            )
+
+    # second load seller_2 data
+    response_order_seller_2 = requests.get(
+        "https://statistics-api.wildberries.ru/api/v1/supplier/orders",
+        headers={"Authorization": token_seller_2},
+        params={"dateFrom": date_from},
+    )
+
+    data_order_seller_2 = response_order_seller_2.json()
+
+    for item in data_order_seller_2:
+        if isinstance(item, dict):
+            srid = item.get("srid")
+            gNumber = item.get("gNumber")
+            date_order = item.get("date")
+            lastchangedate = item.get("lastChangeDate")
+            priceWithDisc = item.get("priceWithDisc")
+            discountPercent = item.get("discountPercent")
+            supplierArticle = item.get("supplierArticle")
+            isCancel = item.get("isCancel")
+            cancelDate = item.get("cancelDate")
+            subject = item.get("subject")
+            category = item.get("category")
+            brand = item.get("brand")
+            owner = "seller_2"
+            ctl_datetime = datetime.now()
+
+            cursor.execute(
+                "INSERT INTO sales (srid, gNumber, date_order, lastchangedate, \
+                                    priceWithDisc,discountPercent, supplierArticle, isCancel, \
+                                    cancelDate, subject, category, brand, owner, ctl_datetime) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                (
+                    srid,
+                    gNumber,
+                    date_order,
+                    lastchangedate,
+                    priceWithDisc,
+                    discountPercent,
+                    supplierArticle,
+                    isCancel,
+                    cancelDate,
+                    subject,
+                    category,
+                    brand,
+                    owner,
                     ctl_datetime,
                 ),
             )
